8815588c71cc690d3188d84dfca5142d555effcc
romeorubiko <raulescabia255@gmail.com>
2018-04-11 13:46:02 +0200

    simbolos terminales

---
+++ pdl_practica2/src/lex/manual/SimbolosTerminales.java
@@ -0,0 +1,20 @@
+package lex.manual;
+
+public class SimbolosTerminales {
+	 /* terminals */
+	 public static final int EOF = 0;
+	 public static final int error = 1;
+	// public static final int nombre = 2;
+	// public static final int numero = 3;
+	// …
+
+	 /* lista de nombres, util para devolver información por pantalla */
+	 public static final String[] terminalNames = new String[] {
+	 "EOF",
+	 "error"
+	// ,"nombre",
+	// "numero"
+	//....
+	 };
+
+}
--- pdl_practica2/src/lex/manual/vo.java
+++
@@ -1,5 +0,0 @@
-package lex.manual;
-
-public class vo {
-
-}
